39f85a854db124f2278c32c3392d4e7a4dfbe99a
Seth Vargo <sethvargo@gmail.com>
2016-01-26 14:52:16 -0500

    Use Consul v0.6.3

--- scripts/install.sh
+++ scripts/install.sh
@@ -7,7 +7,7 @@ sudo apt-get install -y unzip
 
 echo "Fetching Consul..."
 cd /tmp
-curl -L -o consul.zip https://releases.hashicorp.com/consul/0.5.2/consul_0.5.2_linux_amd64.zip
+curl -L -o consul.zip https://releases.hashicorp.com/consul/0.6.3/consul_0.6.3_linux_amd64.zip
 
 echo "Installing Consul..."
 unzip consul.zip >/dev/null
@@ -16,15 +16,6 @@ sudo mv consul /usr/local/bin/consul
 sudo mkdir -p /etc/consul.d
 sudo mkdir -p /mnt/consul
 sudo mkdir -p /etc/service
-
-echo "Fetching Consul UI..."
-cd /tmp
-curl -L -o consul-ui.zip https://releases.hashicorp.com/consul/0.5.2/consul_0.5.2_web_ui.zip
-
-echo "Installing Consul UI..."
-unzip consul-ui.zip >/dev/null
-sudo mkdir -p /mnt/consul-ui
-sudo mv dist/* /mnt/consul-ui/
 
 echo "Installing Upstart service..."
 sudo mv /tmp/upstart.conf /etc/init/consul.conf
